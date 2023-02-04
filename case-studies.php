e0d99af6d278282f8947c7606d49526249b6c40d
Karan <karanchandekar431@gmail.com>
2023-02-04 18:41:26 +0530

    import header and footer

--- case-studies.php
+++ case-studies.php
@@ -1,59 +1,14 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Young Nomads - Your SEO Partner</title>
-
-    <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
-    <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
-
-    <!-- Styles -->
-    <link rel="stylesheet" href="/dist/output.css" />
-    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet" />
-</head>
-
-<body class="bg-no-repeat bg-contain bg-bg-img">
-    <!-- Navbar -->
-    <nav>
-        <a href="/index.html" class="z-20">
-            <!-- <h1 class="text-xl lg:text-3xl">YoungNomads</h1> -->
-            <img class="lg:w-[150px] w-[100px]" src="/images/companies/logo.png" alt="visibility">
-        </a>
-
-        <div class="flex items-center justify-end">
-            <span class="z-20 block mx-2 text-3xl cursor-pointer lg:hidden">
-                <ion-icon onclick="onToggleMenu(this)" name="menu" id="menu-icon""></ion-icon>
-          </span>
-    
-          <div
-            class=" px-4 lg:px-0 bg-no-repeat bg-cover bg-bg-img lg:bg-none z-10 fixed inset-0 w-full
-                    translate-x-[-100%] bg-[#0e1414] lg:bg-transparent shadow-xl transition duration-300 lg:border-r-0
-                    lg:w-auto lg:static lg:shadow-none lg:translate-x-0" id="nav-links">
-                    <div class="flex flex-col h-full lg:space-x-10 lg:items-center lg:flex-row">
-                        <ul class="pt-24 space-y-8 lg:pt-0 lg:space-y-0 lg:flex lg:space-x-10">
-                            <li class="nav-link">
-                                <a href="/index.php">Home</a>
-                            </li>
-                            <li class="nav-link">
-                                <a href="/about.php">About</a>
-                            </li>
-                            <li class="text-indigo-500 nav-link">
-                                <a href="/">Case Studies</a>
-                            </li>
-                            <li class="nav-link">
-                                <a href="/contact.php">Contact</a>
-                            </li>
-                        </ul>
-                        <button id="show-modal" data-modal-target="popup-modal" data-modal-toggle="popup-modal"
-                            class="nav-button" type="button">
-                            FREE AUDIT
-                        </button>
-                    </div>
-        </div>
-    </nav>
+<?php
+/**
+/* Template Name: Case-Studies
+ * 
+ * Display only Case-Studies template
+ * 
+ * @package WordPress
+ * @subpackage visibility
+ * @since visibility 1.0
+*/
+get_header(); ?>
 
     <div id="popup-modal" tabindex="-1"
         class="fixed top-0 left-0 right-0 z-50 flex items-center justify-center hidden w-full h-screen p-0 bg-black bg-opacity-50 md:inset-0">
@@ -354,60 +309,4 @@
         </div>
     </div>
 
-    <!-- footer -->
-    <footer class="container flex-row justify-between md:flex">
-        <div class="links-container">
-            <a href="#">
-                <h1 class="mb-4 text-3xl">YoungNomads</h1>
-            </a>
-            <div class="links-container">
-                <a href="#" class="footer-link"><i class="pr-5 fa-brands fa-linkedin"></i>Linkedin</a>
-                <a href="#" class="footer-link"><i class="pr-5 fa-brands fa-twitter"></i>Twitter</a>
-            </div>
-        </div>
-
-        <div class="links-container">
-            <h4 class="text-xl text-gray-400">Address</h4>
-            <p>We are based out of beautiful city’s new emerging IT hub – Vijay Nagar, Indore, India.</p>
-        </div>
-
-        <div class="links-container">
-            <h4 class="text-xl text-gray-400">Contact No.</h4>
-            <a href="#" class="footer-link">+91 88 273 36 407</a>
-        </div>
-    </footer>
-
-    <!-- Newsletter -->
-    <div class="container flex flex-col items-center justify-between text-center lg:flex-row lg:text-left">
-        <h2 class="mb-8 text-2xl md:text-3xl md:w-96 lg:mb-0">Get the latest updates. Subscribe for our newsletter</h2>
-        <div
-            class="border border-gray-700 rounded-lg flex justify-between content-center p-2 space-x-4 w-full md:w-[600px] lg:w-auto xl:w-[600px]">
-            <input class="w-full p-4 bg-transparent border-none outline-none" type="text"
-                placeholder="Enter your email">
-            <button class="primary-button">
-                <a class="font-bold" href="#">Subscribe</a></button>
-        </div>
-    </div>
-
-    <div class="container py-0">
-        <hr class="mb-20 border border-gray-700">
-    </div>
-
-    <!-- Subfooter -->
-    <div class="container justify-between mb-12 text-center lg:flex">
-        <div class="links-container md:flex-row md:justify-center md:space-y-0 md:space-x-10">
-            <a href="#" class="footer-link">Privacy policy</a>
-            <a href="#" class="footer-link">Terms of Use</a>
-            <a href="#" class="footer-link">Link Policy</a>
-            <a href="#" class="footer-link">FB Policy</a>
-        </div>
-        <h5 class="text-gray-400">Copyright © 2023 Young Nomads | Digital Marketing</h5>
-    </div>
-
-    <!-- Scripts -->
-    <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
-    <script src="https://kit.fontawesome.com/628003722a.js" crossorigin="anonymous"></script>
-    <script src="/js/script.js"></script>
-</body>
-
-</html>
+<?php get_footer(); ?>
