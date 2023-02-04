db5d798cf71eae5deaae2c2c41851fa997655d9b
Karan <karanchandekar431@gmail.com>
2023-02-04 19:24:30 +0530

    added css url

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+/**
+/* The template for displaying the header
+ * 
+ * Display all of the head element and everything up until the "site-content" div.
+ * 
+ * @package WordPress
+ * @subpackage visibility
+ * @since visibility 1.0
+*/
+get_header(); ?>
+
+<?php get_footer(); ?>
